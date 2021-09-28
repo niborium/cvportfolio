5b7b2f879d1652bfbc201541e11a1be434bd959d
Robin Karlsson <robink90@gmail.com>
2021-09-28 12:45:43 +0200

    Lagt in portfolio läses via Github API och renderas ut på sidan

--- script.js
+++ script.js
@@ -12,3 +12,31 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
+
+
+//Läsa in repos via Github API
+
+fetch('https://api.github.com/users/niborium/repos')
+  .then(
+    function(response) {
+        if (response.status !== 200) {
+            console.log('Det verkar vara ett problem. Statuskod: ' +
+               response.status);
+             return;
+           }
+ 
+           // Examine the text in the response
+           response.json().then(function(data) {
+             console.log(data);
+             
+            var githubrepos = document.getElementById('githubrepos')
+            for (i=0; i<data.length; i++){
+              githubrepos.insertAdjacentHTML("beforeend", "<a href='"+ data[i].html_url +"' target='_blank' style='text-decoration:none;''><div class='githubrepo'>" + data[i].name + " {Språk: " + data[i].language + "}" + "</div></a>");
+            }
+
+           });
+        }
+)
+.catch(function(err) {
+  console.log('Fetch Error :-S', err);
+});
