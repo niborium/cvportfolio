f21457cce42278d1bfb56002f1ab41f832ca6f9e
Robin Karlsson <robink90@gmail.com>
2021-12-22 14:29:27 +0100

    indent fix

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var coll = document.getElementsByClassName("minimera");
 var i;
 
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
+  coll[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
     if (content.style.display === "block") {
@@ -13,52 +13,57 @@ for (i = 0; i < coll.length; i++) {
   });
 }
 
-
 //Läsa in repos via Github API
 
-fetch('https://api.github.com/users/niborium/repos')
-  .then(
-    function(response) {
-        if (response.status !== 200) {
-            console.log('Det verkar vara ett problem. Statuskod: ' +
-               response.status);
-             return;
-           }
-           response.json().then(function(data) {        
-            var githubrepos = document.getElementById('githubrepos')
-            for (i=0; i<data.length; i++){
-              githubrepos.insertAdjacentHTML("beforeend", "<a href='"+ data[i].html_url +"' target='_blank' style='text-decoration:none;''><div class='githubrepo'><b>" + data[i].name + "</b><br> {Beskrivning: " + data[i].description + "}" + "</div></a><br><br>");
-            }
+fetch("https://api.github.com/users/niborium/repos")
+  .then(function (response) {
+    if (response.status !== 200) {
+      console.log("Det verkar vara ett problem. Statuskod: " + response.status);
+      return;
+    }
+    response.json().then(function (data) {
+      var githubrepos = document.getElementById("githubrepos");
+      for (i = 0; i < data.length; i++) {
+        githubrepos.insertAdjacentHTML(
+          "beforeend",
+          "<a href='" +
+            data[i].html_url +
+            "' target='_blank' style='text-decoration:none;''><div class='githubrepo'><b>" +
+            data[i].name +
+            "</b><br> {Beskrivning: " +
+            data[i].description +
+            "}" +
+            "</div></a><br><br>"
+        );
+      }
+    });
+  })
+  .catch(function (err) {
+    console.log("Fetch Error :-S", err);
+  });
 
-           });
-        }
-)
-.catch(function(err) {
-  console.log('Fetch Error :-S', err);
-});
-
-function Ommigstart(){
-var myElement = document.getElementById('Ommigstart');
-var topPos = myElement.offsetTop;
-document.getElementById('Ommigstart').scrollTop = topPos;
+function Ommigstart() {
+  var myElement = document.getElementById("Ommigstart");
+  var topPos = myElement.offsetTop;
+  document.getElementById("Ommigstart").scrollTop = topPos;
 }
-function Arblfstart(){
-var myElement = document.getElementById('Arblfstart');
-var topPos = myElement.offsetTop;
-document.getElementById('Arblfstart').scrollTop = topPos;
+function Arblfstart() {
+  var myElement = document.getElementById("Arblfstart");
+  var topPos = myElement.offsetTop;
+  document.getElementById("Arblfstart").scrollTop = topPos;
 }
-function Utbstart(){
-var myElement = document.getElementById('Utbstart');
-var topPos = myElement.offsetTop;
-document.getElementById('Utbstart').scrollTop = topPos;
+function Utbstart() {
+  var myElement = document.getElementById("Utbstart");
+  var topPos = myElement.offsetTop;
+  document.getElementById("Utbstart").scrollTop = topPos;
 }
-function CoKstart(){
-  var myElement = document.getElementById('CoKstart');
-var topPos = myElement.offsetTop;
-document.getElementById('CoKstart').scrollTop = topPos;
+function CoKstart() {
+  var myElement = document.getElementById("CoKstart");
+  var topPos = myElement.offsetTop;
+  document.getElementById("CoKstart").scrollTop = topPos;
 }
-function Portstart(){
-var myElement = document.getElementById('Portstart');
-var topPos = myElement.offsetTop;
-document.getElementById('Portstart').scrollTop = topPos;
+function Portstart() {
+  var myElement = document.getElementById("Portstart");
+  var topPos = myElement.offsetTop;
+  document.getElementById("Portstart").scrollTop = topPos;
 }
